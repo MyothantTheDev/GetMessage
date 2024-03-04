32b996b940b6dec5609be76bb6309d6f17a6f1e3
Myo Thant <003.myothant7729@gmail.com>
2024-03-04 12:18:06 +0800

    init react native

---
+++ screens/LoginScreen.js
@@ -0,0 +1,71 @@
+import { View, Text, Image, StatusBar, TextInput, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import Animated, {FadeInUp, FadeInDown} from "react-native-reanimated";
+
+export default function LoginScreen() {
+
+  const disableCLick = useRef(true);
+
+  const [confidential, setConfidential] = useState({email: '', password: ''});
+  const inputConfidential = (item, value) => {
+    const input_value = item === 'email' ? {email: value} : {password: value};
+    setConfidential(confidential => ({
+      ...confidential,
+      ...input_value
+    }))
+  }
+  const enableClick = (value) => {
+    if (value.length >= 8) {
+      disableCLick.current = false;
+    } else {
+      disableCLick.current = true;
+    }
+  }
+
+  return (
+    <View className="bg-white w-full h-full">
+      <StatusBar style="light" />
+      <Image className="h-full w-full absolute" source={require('../assets/background.png')} />
+
+      {/* light */}
+      <View className="absolute flex-row justify-around w-full">
+        <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-255 w-90" source={require('../assets/light.png')} />
+        <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require('../assets/light.png')} />
+      </View>
+
+      {/* title and form */}
+
+      <View className="h-full w-full flex justify-around pt-40 pb-10">
+        <View className="flex items-center">
+          <Animated.Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider text-4xl">
+            Portfolio Messager
+          </Animated.Text>
+        </View>
+
+        {/* form */}
+        <View className="flex items-center mx-4 space-y-4">
+          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder="Email" placeholderTextColor={'grey'} onChangeText={text => inputConfidential('email', text)}/>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+            <TextInput placeholder="Password" placeholderTextColor={'grey'} secureTextEntry 
+              onChangeText={text => {
+                inputConfidential('password', text);
+                enableClick(text);
+              }}/>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="w-full">
+            <TouchableOpacity 
+              className={`w-full p-3 rounded-2xl ${disableCLick.current ? 'bg-gray-400' : 'bg-blue-400'} items-center`}
+              disabled={disableCLick.current}
+             >
+              <Text className="font-bold text-white text-lg">
+                Login
+              </Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </View>
+    </View>
+  )
+}
